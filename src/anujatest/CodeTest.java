52dd182117f336dbdce4fba11eedbfab969a7281
ART24 <ART24@ART241318>
2024-03-26 23:16:15 +0530

    Code test

---
+++ src/anujatest/CodeTest.java
@@ -0,0 +1,92 @@
+package anujatest;
+
+public class CodeTest {
+	public static void main(String[] args) {
+		// Case 1:
+		int i = 10;
+		int j= 6 ;
+		int sum = i+j;
+		int difference = i-j;
+	    int product = i*j;
+		int average = (i+j)/2;
+		int distance = i-j;
+		int max =  i+j-j;
+		int min = i+j-i;
+		System.out.println("Sum:"+ sum);
+		System.out.println("Difference:" + difference);
+		System.out.println("Product:"+ product);
+		System.out.println("Average:" + average);
+		System.out.println("Distanc:" + distance);
+		System.out.println("Max:"+ max);
+		System.out.println("Min:" + min);
+		// Case 2:
+			int m = -9;
+			int n= 10 ;
+			int sum1 = m+n;
+			int difference1 = m-n;
+		    int product1 = m*n;
+			int average1 = (m+n)/2;
+			int distance1 = m-n;
+			int max1 =  m+n-m;
+			int min1 = m+n-n;
+			System.out.println("Sum:"+ sum1);
+			System.out.println("Difference:" + difference1);
+			System.out.println("Product:"+ product1);
+			System.out.println("Average:" + average1);
+			System.out.println("Distanc:" + distance1);
+			System.out.println("Max:"+ max1);
+			System.out.println("Min:" + min1);
+			//Case3
+			int h = 4;
+			int k = 10 ;
+			int sum2 = h+k;
+			int difference2 = h-k;
+		    int product2 = h*k;
+			int average2 = (h+k)/2;
+			int distance2 = h-k;
+			int max2 =  h+k-h;
+			int min2 = h+k-k;
+			System.out.println("Sum:"+ sum2);
+			System.out.println("Difference:" + difference2);
+			System.out.println("Product:"+ product2);
+			System.out.println("Average:" + average2);
+			System.out.println("Distanc:" + distance2);
+			System.out.println("Max:"+ max2);
+			System.out.println("Min:" + min2);
+			//Case4:
+			double o = 9.8;
+			double s = 5.6 ;
+			double sum3 = h+k;
+			double difference3 = o-s;
+		    double product3 = o*s;
+			double average3 = (o+s)/2;
+			double distance3 = o-s;
+			double max3 =  o+s-s;
+			double min3 = o+s-o;
+			System.out.println("Sum:"+ sum3);
+			System.out.println("Difference:" + difference3);
+			System.out.println("Product:"+ product3);
+			System.out.println("Average:" + average3);
+			System.out.println("Distanc:" + distance3);
+			System.out.println("Max:"+ max3);
+			System.out.println("Min:" + min3);
+			//Case5 
+			double p = 3/4;
+			double q = 6/9 ;
+			double sum4 = p+q;
+			double difference4 = p-q;
+		    double product4 = p*q;
+			double average4 = (p+q)/2;
+			double distance4 = p-q;
+			double max4 =  p+q-q;
+			double min4 = p+q-p;
+			System.out.println("Sum:"+ sum4);
+			System.out.println("Difference:" + difference4);
+			System.out.println("Product:"+ product4);
+			System.out.println("Average:" + average4);
+			System.out.println("Distanc:" + distance4);
+			System.out.println("Max:"+ max4);
+			System.out.println("Min:" + min4);
+
+}
+}
