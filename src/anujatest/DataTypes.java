596b1773c81cf22c09b2cb33d1a8789dc72742aa
ART24 <ART24@ART241318>
2024-03-26 22:07:51 +0530

    Marathi Name

---
+++ src/anujatest/DataTypes.java
@@ -0,0 +1,8 @@
+package anujatest;
+
+public class DataTypes {
+	public static void main(String[] args)   {
+	String marathiName = "\u0905\u0928\u0941\u091c\u093e";
+	System.out.println("My marathi Name is :" + marathiName);
+}	
+}
